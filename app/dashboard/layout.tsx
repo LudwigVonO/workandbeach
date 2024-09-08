708b1db5fc6032e6aa025e810341237ebe47c613
LudwigVonO <louis.strauss@ehl.ch>
2024-09-08 19:31:50 +0200

    first push

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import SideNav from "../ui/dashboard/sidenav";
+
+function Layout({children}:{children : React.ReactNode}){
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md: w-64">
+        <SideNav />
+      </div>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Climate_Crisis, Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const climateCrisis = Climate_Crisis({ subsets: ['latin'] });
+export const lusitana = Lusitana({weight:"700",subsets:['latin']});
+
