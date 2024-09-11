c6d79b5ccd7dfccb87f7d9437746a25aab4e72af
LudwigVonO <louis.strauss@ehl.ch>
2024-09-11 21:50:20 +0200

    Test: Deployment

--- auth.config.ts
+++ auth.config.ts
@@ -10,19 +10,19 @@ export const authConfig = {
       console.log('user',auth?.user)
       const isOnAdmin = nextUrl.pathname.startsWith('/admin');
       if (isOnAdmin){
-        if (isLoggedIn && (auth.user?.email == 'strausslouis95@gmail.com' || auth.user?.email == 'user@nextmail.com')){
+        // if (isLoggedIn && (auth.user?.email == 'strausslouis95@gmail.com' || auth.user?.email == 'user@nextmail.com')){
+        // if (isLoggedIn){
           return true
-        }
-        console.log('long live the liver lover')
-        return false;
+        // }        
+        // return false;
       }
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
-      }
+      // if (isOnDashboard) {
+      //   if (isLoggedIn) return true;
+      //   return false; // Redirect unauthenticated users to login page
+      // } else if (isLoggedIn) {
+      //   return Response.redirect(new URL('/dashboard', nextUrl));
+      // }
       return true;
     },
   },
